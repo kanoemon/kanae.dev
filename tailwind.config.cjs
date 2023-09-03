8541ec5b5dd2a6d7daa0c011c1ab68e4e0f113f2
Kanae Suzuki <kanoemon.cho@gmail.com>
2023-09-03 15:24:30 +0900

    change design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+			  DEFAULT: {
+				css: {
+				  a: {
+					textDecoration: "none",
+					borderBotom: 4,
+					fontWeight: "bold",
+				  },
+				},
+			  },
+			},
+		  },
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
